99884ee4da1d6b3dfd127c5cc23515471788ddbb
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-02-20 10:51:55 +0530

    Created Speciality menu

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../componants/Header";
+import SpecialityMenu from "../componants/SpecialityMenu";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <SpecialityMenu />
     </div>
   );
 };
