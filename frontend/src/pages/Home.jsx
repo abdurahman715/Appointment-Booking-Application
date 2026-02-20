0d3a1b4bdba40cd1161195aeb5aea02ad383f5df
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-02-20 12:22:56 +0530

    Created Top Doctors list

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../componants/Header";
 import SpecialityMenu from "../componants/SpecialityMenu";
+import TopDoctors from "../componants/TopDoctors";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <SpecialityMenu />
+      <TopDoctors />
     </div>
   );
 };
